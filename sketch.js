1180655c669e777f45026527cfae755993427d5c
Rohan Smith <rohan.smith@nelyx.net>
2018-03-24 14:59:14 +1000

    Added some training data and trained the model.

--- sketch.js
+++ sketch.js
@@ -1,5 +1,26 @@
 var nn;
 
+var training_data_or = [
+  { inputs : [0,0], outputs: [0]},
+  { inputs : [0,1], outputs: [1]},
+  { inputs : [1,0], outputs: [1]},
+  { inputs : [1,1], outputs: [1]},
+];
+
+var training_data_and = [
+  { inputs : [0,0], outputs: [0]},
+  { inputs : [0,1], outputs: [0]},
+  { inputs : [1,0], outputs: [0]},
+  { inputs : [1,1], outputs: [1]},
+];
+
+var training_data_xor = [
+  { inputs : [0,0], outputs: [0]},
+  { inputs : [0,1], outputs: [1]},
+  { inputs : [1,0], outputs: [1]},
+  { inputs : [1,1], outputs: [0]},
+];
+
 
 var res = 20;
 var cols, rows;
@@ -12,5 +33,11 @@ function setup() {
 }
 
 function draw() {
+  var training_data = training_data_xor;
 
+  for (var i = 0; i < 1000; i++) {
+    var data = random(training_data);
+    nn.train(data.inputs, data.outputs);
+  }
+  
 }
